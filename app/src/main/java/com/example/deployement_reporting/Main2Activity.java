6235832e8ec31f54bef62e8bf975769437a7c8ad
Playdric <cedric.nanteau@gmail.com>
2018-04-03 14:11:22 +0200

    cour fini

--- app/src/main/java/com/example/deployement_reporting/Main2Activity.java
+++ app/src/main/java/com/example/deployement_reporting/Main2Activity.java
@@ -3,11 +3,14 @@ package com.example.deployement_reporting;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.microsoft.appcenter.analytics.Analytics;
+
 public class Main2Activity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+        Analytics.trackEvent("My custom event2");
     }
 }
--- app/src/main/java/com/example/deployement_reporting/MainActivity.java
+++ app/src/main/java/com/example/deployement_reporting/MainActivity.java
@@ -6,11 +6,15 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import com.crashlytics.android.Crashlytics;
+import com.google.firebase.analytics.FirebaseAnalytics;
 import com.microsoft.appcenter.AppCenter;
 import com.microsoft.appcenter.analytics.Analytics;
 import com.microsoft.appcenter.crashes.Crashes;
 
 import butterknife.BindView;
+import butterknife.ButterKnife;
+import io.fabric.sdk.android.Fabric;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -20,15 +24,20 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Fabric.with(this, new Crashlytics());
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
         AppCenter.start(getApplication(), "eb105f6a-2dc4-4599-80fb-3c32c01eb92a",
                 Analytics.class, Crashes.class);
         Analytics.trackEvent("My custom event");
+        FirebaseAnalytics.getInstance(this).logEvent("bonjour", null);
 
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                startActivity(new Intent(MainActivity.this, Main2Activity.class));
+                //startActivity(new Intent(MainActivity.this, Main2Activity.class));
+                Crashlytics.getInstance().crash();
             }
         });
     }
