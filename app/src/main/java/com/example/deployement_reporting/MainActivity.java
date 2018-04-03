e733c81a386f61a35b68b12b712f8a6bd22376d7
Playdric <cedric.nanteau@gmail.com>
2018-04-03 11:07:46 +0200

    add new qctivity + butterknife

--- app/src/main/java/com/example/deployement_reporting/MainActivity.java
+++ app/src/main/java/com/example/deployement_reporting/MainActivity.java
@@ -1,12 +1,21 @@
 package com.example.deployement_reporting;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
 import com.microsoft.appcenter.AppCenter;
 import com.microsoft.appcenter.analytics.Analytics;
 import com.microsoft.appcenter.crashes.Crashes;
 
+import butterknife.BindView;
+
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.btnButton)
+    public Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,5 +24,12 @@ public class MainActivity extends AppCompatActivity {
         AppCenter.start(getApplication(), "eb105f6a-2dc4-4599-80fb-3c32c01eb92a",
                 Analytics.class, Crashes.class);
         Analytics.trackEvent("My custom event");
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, Main2Activity.class));
+            }
+        });
     }
 }
